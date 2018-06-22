f28577332d9ff9de0bc4a442943576c91167d175
AnthonyAmanse <ghieamanse@gmail.com>
2018-06-22 11:42:36 -0700

    Add more options in findAll
    This commit adds in options:
    * ORDER BY clause for sorting
    * OFFSET and LIMIT for retrieving a portion of the rows
    
    This builds the appropriate SQL queries for the options listed above.
    This commit also adds in initial tests for the new options.
    
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- Tests/SwiftKueryORMTests/TestFind.swift
+++ Tests/SwiftKueryORMTests/TestFind.swift
@@ -10,6 +10,10 @@ class TestFind: XCTestCase {
             ("testFind", testFind),
             ("testFindAll", testFindAll),
             ("testFindAllMatching", testFindAllMatching),
+            ("testFindAllLimit", testFindAllLimit),
+            ("testFindAllLimitAndOffset", testFindAllLimitAndOffset),
+            ("testFindAllOrderByDescending", testFindAllOrderByDescending),
+            ("testFindAllOrderByAscending", testFindAllOrderByAscending),
         ]
     }
 
@@ -108,6 +112,119 @@ class TestFind: XCTestCase {
                   let user = array[0]
                   XCTAssertEqual(user.name, "Joe")
                   XCTAssertEqual(user.age, 38)
+                }
+                expectation.fulfill()
+            }
+        })
+    }
+
+
+    /**
+      Testing that the correct SQL Query is created to retrieve a specific model.
+      Testing that the model can be retrieved
+    */
+    func testFindAllLimit() {
+        let connection: TestConnection = createConnection(.returnOneRow)
+        Database.default = Database(single: connection)
+        performTest(asyncTasks: { expectation in
+            Person.findAll(limit: 1) { array, error in
+                XCTAssertNil(error, "Find Failed: \(String(describing: error))")
+                XCTAssertNotNil(connection.query, "Find Failed: Query is nil")
+                if let query = connection.query {
+                  let expectedQuery = "SELECT * FROM \"People\" LIMIT 1"
+                  let resultQuery = connection.descriptionOf(query: query)
+                  XCTAssertEqual(resultQuery, expectedQuery, "Find Failed: Invalid query")
+                }
+                XCTAssertNotNil(array, "Find Failed: No array of models returned")
+                if let array = array {
+                  XCTAssertEqual(array.count, 1, "Find Failed: \(String(describing: array.count)) is not equal to 1")
+                }
+                expectation.fulfill()
+            }
+        })
+    }
+
+    /**
+      Testing that the correct SQL Query is created to retrieve a specific model.
+      Testing that the model can be retrieved
+    */
+    func testFindAllLimitAndOffset() {
+        let connection: TestConnection = createConnection(.returnOneRow)
+        Database.default = Database(single: connection)
+        performTest(asyncTasks: { expectation in
+            Person.findAll(offset: 2, limit: 1) { array, error in
+                XCTAssertNil(error, "Find Failed: \(String(describing: error))")
+                XCTAssertNotNil(connection.query, "Find Failed: Query is nil")
+                if let query = connection.query {
+                  let expectedQuery = "SELECT * FROM \"People\" LIMIT 1 OFFSET 2"
+                  let resultQuery = connection.descriptionOf(query: query)
+                  XCTAssertEqual(resultQuery, expectedQuery, "Find Failed: Invalid query")
+                }
+                XCTAssertNotNil(array, "Find Failed: No array of models returned")
+                if let array = array {
+                  XCTAssertEqual(array.count, 1, "Find Failed: \(String(describing: array.count)) is not equal to 1")
+                }
+                expectation.fulfill()
+            }
+        })
+    }
+
+    /**
+      Testing that the correct SQL Query is created to retrieve a specific model.
+      Testing that correct amount of models are retrieved
+      Testing that models are sorted by age in descending order
+    */
+    func testFindAllOrderByDescending() {
+        let connection: TestConnection = createConnection(.returnThreeRowsSortedDescending)
+        Database.default = Database(single: connection)
+        performTest(asyncTasks: { expectation in
+            Person.findAll(order: Order.desc("age")) { array, error in
+                XCTAssertNil(error, "Find Failed: \(String(describing: error))")
+                XCTAssertNotNil(connection.query, "Find Failed: Query is nil")
+                if let query = connection.query {
+                  let expectedQuery = "SELECT * FROM \"People\" ORDER BY \"People\".\"age\" DESC"
+                  let resultQuery = connection.descriptionOf(query: query)
+                  XCTAssertEqual(resultQuery, expectedQuery, "Find Failed: Invalid query")
+                }
+                XCTAssertNotNil(array, "Find Failed: No array of models returned")
+                if let array = array {
+                  for (index, person) in array.enumerated() {
+                    if index + 1 < array.count {
+                      XCTAssertGreaterThanOrEqual(person.age, array[index + 1].age, "Find Failed: Age of person: \(String(describing: person.age)) is not greater than or equal to age of next person: \(String(describing: array[index + 1].age))")
+                    }
+                  }
+                  XCTAssertEqual(array.count, 3, "Find Failed: \(String(describing: array.count)) is not equal to 3")
+                }
+                expectation.fulfill()
+            }
+        })
+    }
+
+    /**
+      Testing that the correct SQL Query is created to retrieve a specific model.
+      Testing that correct amount of models are retrieved
+      Testing that models are sorted by age in ascending order
+    */
+    func testFindAllOrderByAscending() {
+        let connection: TestConnection = createConnection(.returnThreeRowsSortedAscending)
+        Database.default = Database(single: connection)
+        performTest(asyncTasks: { expectation in
+            Person.findAll(order: Order.asc("age")) { array, error in
+                XCTAssertNil(error, "Find Failed: \(String(describing: error))")
+                XCTAssertNotNil(connection.query, "Find Failed: Query is nil")
+                if let query = connection.query {
+                  let expectedQuery = "SELECT * FROM \"People\" ORDER BY \"People\".\"age\" ASC"
+                  let resultQuery = connection.descriptionOf(query: query)
+                  XCTAssertEqual(resultQuery, expectedQuery, "Find Failed: Invalid query")
+                }
+                XCTAssertNotNil(array, "Find Failed: No array of models returned")
+                if let array = array {
+                  for (index, person) in array.enumerated() {
+                    if index + 1 < array.count {
+                      XCTAssertLessThanOrEqual(person.age, array[index + 1].age, "Find Failed: Age of person: \(String(describing: person.age)) is not less than or equal to age of next person: \(String(describing: array[index + 1].age))")
+                    }
+                  }
+                  XCTAssertEqual(array.count, 3, "Find Failed: \(String(describing: array.count)) is not equal to 3")
                 }
                 expectation.fulfill()
             }
