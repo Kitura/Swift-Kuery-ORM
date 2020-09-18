8eba95b3bf3bb978a31fff900f09d0c67f207842
Fouad Hatem <fouadhatem@icloud.com>
2020-09-18 19:38:19 +0300

    Update repo references
    Update repo references from IBM-Swift to Kitura.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /**
- * Copyright IBM Corporation 2016-2019
+ * Copyright IBM Corporation and the Kitura project authors 2016-2020
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,9 +30,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url:"https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.0.0"),
-        .package(url:"https://github.com/IBM-Swift/KituraContracts.git", from: "1.0.0"),
-        .package(url:"https://github.com/IBM-Swift/TypeDecoder.git", from: "1.0.0")
+        .package(url:"https://github.com/Kitura/Swift-Kuery.git", from: "3.0.0"),
+        .package(url:"https://github.com/Kitura/KituraContracts.git", from: "1.0.0"),
+        .package(url:"https://github.com/Kitura/TypeDecoder.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
